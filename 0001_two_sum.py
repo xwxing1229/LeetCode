0c89294893a45f9dee9ac97586272be7594cbff2
Xiaowei Xing <997427575@qq.com>
2021-01-20 11:23:31 +0900

    Updated

--- 0001_two_sum.py
+++ 0001_two_sum.py
@@ -6,4 +6,4 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     res = [i,j]
-        return res
+        return res
