9c74b0e2f67e841303ae436adc916a3e01c2ac87
Xiaowei Xing <997427575@qq.com>
2021-01-21 22:02:16 +0900

    Add files via upload

---
+++ 69_sqrtx.py
@@ -0,0 +1,14 @@
+# https://leetcode-cn.com/problems/sqrtx/
+
+class Solution:
+    def mySqrt(self, x:int) -> int:
+        left = 0
+        right = x
+        
+        while left <= right:
+            mid = (left+right) // 2
+            if mid * mid <= x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left-1
