be204cc82fe504c44b6ce7dcdcab29acc6012180
Everglow0214 <997427575@qq.com>
2022-03-27 17:58:03 +0900

    Cpp practice

---
+++ Cpp/0001_two_sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> table;
+		for (int i = 0; i < nums.size(); ++i) {
+			auto iter = table.find(target - nums[i]);
+			if (iter != table.end()) {
+				return {iter->second, i};
+			}
+			table.emplace(nums[i], i);
+		}
+		return {};
+	}
+};
