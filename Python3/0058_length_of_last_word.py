1e03cb8a6a70fb62770b2e7ece84bf3e97f4ba83
Everglow0214 <997427575@qq.com>
2022-06-08 18:56:31 +0900

    Update 0058

--- Python3/0058_length_of_last_word.py
+++ Python3/0058_length_of_last_word.py
@@ -1,18 +1,22 @@
-# https://leetcode-cn.com/problems/length-of-last-word/
+# https://leetcode.cn/problems/length-of-last-word/
 
 class Solution:
     def lengthOfLastWord(self, s:str) -> int:
-        idx = len(s) - 1
-        while idx > 0:
-            if s[idx] != ' ':
-                break
-            else:
-                idx = idx - 1
-        
-        for i in range(idx, -1, -1):
-            if s[i] == ' ':
-                res = idx - i
-                break
-            if i == 0:
-                res = idx + 1
+        """
+        Inputs:
+            s: str
+        Outputs:
+            res: int
+        """
+
+        s = " " + s + " "
+        res = 0
+        tmp = 0
+        for i in range(1, len(s)):
+            c = s[i]
+            if c != " ":
+                tmp = tmp + 1
+            elif s[i-1] != " ":
+                res = tmp
+                tmp = 0
         return res
