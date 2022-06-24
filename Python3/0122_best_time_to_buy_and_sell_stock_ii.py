6ef7373ac9296672a89771de12c34cb1bc152b80
Everglow0214 <997427575@qq.com>
2022-06-24 22:19:15 +0900

    Update 0122

--- Python3/0122_best_time_to_buy_and_sell_stock_ii.py
+++ Python3/0122_best_time_to_buy_and_sell_stock_ii.py
@@ -1,4 +1,4 @@
-# https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+# https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
 
 class Solution:
     def maxProfit(self, prices):
@@ -10,23 +10,18 @@ class Solution:
         """
         
         '''
-        res = []
-        profit = 0
-        buy = prices[0]
-        prices.append(0)
-        for price in prices:
-            if price - buy < profit:
-                res.append(profit)
-                buy = price
-                profit = 0
-            else:
-                profit = price - buy
-        return sum(res)
-        '''
-        
         n = len(prices)
         dp = [0 for _ in range(n)]
         for i in range(1,n):
             # dp[i] indicates the maximum profit till now.
             dp[i] = dp[i-1] + max(0, prices[i]-prices[i-1])
         return dp[-1]
+        '''
+
+        res = 0
+        for i in range(1, len(prices)):
+            yesterday = prices[i-1]
+            today = prices[i]
+            if yesterday < today:
+                res = res + today - yesterday
+        return res
