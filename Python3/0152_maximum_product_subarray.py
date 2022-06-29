af1b4d59ba73ac2fe05f5ab4d7bd5f923cc5dc50
Everglow0214 <997427575@qq.com>
2022-06-29 21:32:20 +0900

    Update 0152

--- Python3/0152_maximum_product_subarray.py
+++ Python3/0152_maximum_product_subarray.py
@@ -1,4 +1,4 @@
-# https://leetcode-cn.com/problems/maximum-product-subarray/
+# https://leetcode.cn/problems/maximum-product-subarray/
 
 class Solution:
     def maxProduct(self, nums):
@@ -9,6 +9,7 @@ class Solution:
             res: int
         """
 
+        """
         n = len(nums)
         dp_max = [0 for i in range(n)]
         dp_min = [0 for i in range(n)]
@@ -20,4 +21,15 @@ class Solution:
             dp_min[i] = min(nums[i], dp_max[i-1]*nums[i], dp_min[i-1]*nums[i])
 
         res = max(dp_max)
+        return res
+        """
+
+        res = nums[0]
+        tmp_max = nums[0]
+        tmp_min = nums[0]
+        for i in range(1, len(nums)):
+            tmp = max(tmp_max*nums[i], max(tmp_min*nums[i], nums[i]))
+            tmp_min = min(tmp_max*nums[i], min(tmp_min*nums[i], nums[i]))
+            tmp_max = tmp
+            res = max(res, tmp_max)
         return res
