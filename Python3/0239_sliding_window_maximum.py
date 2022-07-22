d6623cb4fda81b2b55f5838fa56cd57ce6e0979f
Everglow0214 <997427575@qq.com>
2022-07-22 21:00:23 +0900

    Add 0239

--- Python3/0239_sliding_window_maximum.py
+++ Python3/0239_sliding_window_maximum.py
@@ -1,4 +1,4 @@
-# https://leetcode-cn.com/problems/sliding-window-maximum/
+# https://leetcode.cn/problems/sliding-window-maximum/
 
 class Solution:
     def maxSlidingWindow(self, nums, k):
@@ -10,27 +10,60 @@ class Solution:
             res: list[int]
         """
         
-        # Indexes of a nondecreasing sequence are recorded
-        # in queue.
-        queue = []
+        # # Indexes of a decreasing sequence are recorded in queue.
+        # queue = []
+        # res = []
+        # for i in range(len(nums)):
+        #     # While the number corresponding to the last element
+        #     # of queue is less than or equal to nums[i], remove the
+        #     # index of this number, which is queue[-1].
+        #     while queue and nums[queue[-1]] <= nums[i]:
+        #         queue.pop()
+        #     queue.append(i)
+            
+        #     # If the leftmost element in queue is out of the current
+        #     # window, remove this element, which is queue[0].
+        #     while queue and queue[0] < i - k + 1:
+        #         queue.pop(0)
+            
+        #     # Add the largest value of the current window to res,
+        #     # starting from i = k - 1.
+        #     if i >= k - 1:
+        #         res.append(nums[queue[0]])
+        # return res
+
+
+        # res = []
+        # window = []
+        # for i in range(k):
+        #     heapq.heappush(window, (-nums[i], i))
+        # res.append(-window[0][0])
+
+        # for i in range(k, len(nums)):
+        #     heapq.heappush(window, (-nums[i], i))
+        #     while (i-k+1 > window[0][1]):
+        #         heapq.heappop(window)
+
+        #     res.append(-window[0][0])
+        # return res
+
+
         res = []
-        
-        n = len(nums)
-        for i in range(n):
-            # While the number corresponding to the last element
-            # of queue is less than nums[i], remove the index of
-            # this number, which is queue[-1].
-            while queue and nums[queue[-1]] < nums[i]:
-                queue.pop()
-            queue.append(i)
-            
-            # If the leftmost element in queue is out of the current
-            # window, remove this element, which is queue[0].
-            if queue and queue[0] < i - k + 1:
-                queue.pop(0)
-            
-            # Add the largest value of the current window to res,
-            # starting from i = k - 1.
-            if i >= k - 1:
-                res.append(queue[0])
+        deq = [] # deq saves indices
+        for i in range(k):
+            while (deq) and (nums[deq[-1]] <= nums[i]):
+                deq.pop()
+            deq.append(i)
+
+        res.append(nums[deq[0]])
+
+        for i in range(k, len(nums)):
+            while (deq) and (nums[deq[-1]] <= nums[i]):
+                deq.pop()
+            deq.append(i)
+
+            while i-k+1 > deq[0]:
+                deq.pop(0)
+
+            res.append(nums[deq[0]])
         return res
