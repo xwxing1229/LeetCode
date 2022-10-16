104a2797e0c5c2fdcf40f0ff7ad809ed6f874e7d
Everglow0214 <997427575@qq.com>
2022-10-16 22:01:41 +0900

    Update 0414

--- Python3/0414_third_maximum_number.py
+++ Python3/0414_third_maximum_number.py
@@ -1,4 +1,4 @@
-# https://leetcode-cn.com/problems/third-maximum-number/
+# https://leetcode.cn/problems/third-maximum-number/
 
 class Solution:
     def thirdMax(self, nums):
@@ -9,6 +9,24 @@ class Solution:
             res: int
         """
 
+        s = set(nums)
+        max_1, max_2, max_3 = float("-inf"), float("-inf"), float("-inf")
+        for i in s:
+            if i > max_1:
+                max_3 = max_2
+                max_2 = max_1
+                max_1 = i
+            elif i > max_2:
+                max_3 = max_2
+                max_2 = i
+            elif i > max_3:
+                max_3 = i
+        if len(s) < 3:
+            return max_1
+        else:
+            return max_3
+
+        """
         nums_new = list(set(nums))
         n = len(nums_new)
         if n < 3:
@@ -18,3 +36,4 @@ class Solution:
         nums_new.pop()
         nums_new.pop()
         return nums_new[n-3]
+        """
