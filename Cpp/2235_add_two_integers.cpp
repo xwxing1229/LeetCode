14d5997c537936e4908f924c9d1614b5cd382a72
Xing <997427575@qq.com>
2023-08-19 15:52:25 +0900

    Add 2235

---
+++ Cpp/2235_add_two_integers.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/add-two-integers/
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
---
+++ Python3/2235_add_two_integers.py
@@ -0,0 +1,11 @@
+# https://leetcode.cn/problems/add-two-integers/
+
+class Solution:
+    def sum(self, num1, num2):
+        """
+        Inputs:
+            num1, num2: int
+        Outputs:
+            res: int
+        """
+        return num1 + num2
