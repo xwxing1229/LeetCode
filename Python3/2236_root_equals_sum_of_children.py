fafcc758092c34f18e902edce3c277e8ca7adb9f
Xing <997427575@qq.com>
2023-08-21 22:33:16 +0900

    Add 2236

---
+++ Python3/2236_root_equals_sum_of_children.py
@@ -0,0 +1,11 @@
+# https://leetcode.cn/problems/root-equals-sum-of-children/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return True if root.val == root.left.val + root.right.val else False
