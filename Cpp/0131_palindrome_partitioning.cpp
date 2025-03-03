adb04862dc24ac9665d80bf7f4c217d541b66fbe
Xing <997427575@qq.com>
2025-03-03 13:49:29 +0900

    Update

--- Cpp/0131_palindrome_partitioning.cpp
+++ Cpp/0131_palindrome_partitioning.cpp
@@ -1,43 +1,59 @@
 // https://leetcode.cn/problems/palindrome-partitioning/
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<string> tmp;
-        backtrack(s, tmp, 0);
-        return res;
+        vector<string> cur;
+        dfs(cur, s, 0);
+        return res_;
     }
 
-private:
-    void backtrack(string s, vector<string> &tmp, int idx) {
-        if (idx == s.size()) {
-            res.push_back(tmp);
+    void dfs(vector<string> &cur, string &s, int idx) {
+        int n = s.size();
+        if (idx == n) {
+            res_.push_back(cur);
             return;
         }
-
-        for (int length = 1; length < s.size()-idx+1; ++length) {
-            string substr = s.substr(idx, length);
-            if (isValid(substr)) {
-                tmp.push_back(substr);
-                backtrack(s, tmp, idx+length);
-                tmp.pop_back();
+        for (int i = idx; i < n; ++i) {
+            int left = idx, right = i;
+            bool valid = true;
+            while (left < right) {
+                if (s[left] == s[right]) {
+                    left += 1;
+                    right -= 1;
+                }
+                else {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                cur.push_back(s.substr(idx, i-idx+1));
+                dfs(cur, s, i+1);
+                cur.pop_back();
             }
         }
     }
 
-    bool isValid(string s) {
-        int i = 0, j = s.size() - 1;
-        while (i < j) {
-            if (s[i] == s[j]) {
-                i = i + 1;
-                j = j - 1;
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    vector<vector<string>> res;
+    vector<vector<string>> res_;
 };
+
+int main() {
+    Solution solu;
+    string s = "aba";
+    vector<vector<string>> res = solu.partition(s);
+    for (vector<string> row: res) {
+        for (string st: row) {
+            cout << st << ", ";
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
--- Python3/0131_palindrome_partitioning.py
+++ Python3/0131_palindrome_partitioning.py
@@ -9,28 +9,26 @@ class Solution:
             res: list[list[str]]
         """
 
-        self.res = []
-        self.backtrack(s, [], 0)
-        return self.res
+        def dfs(cur, idx):
+            if idx == n:
+                res.append(cur.copy())
+                return
+            for i in range(idx, n):
+                valid = True
+                left, right = idx, i
+                while left < right:
+                    if s[left] == s[right]:
+                        left += 1
+                        right -= 1
+                    else:
+                        valid = False
+                        break
+                if valid:
+                    cur.append(s[idx:i+1])
+                    dfs(cur, i+1)
+                    cur.pop()
 
-    def backtrack(self, s, tmp, idx):
-        if idx == len(s):
-            self.res.append(tmp)
-            return
-
-        for length in range(1, len(s)-idx+1):
-            substr = s[idx: idx+length]
-            if self.isValid(substr):
-                self.backtrack(s, tmp+[substr], idx+length)
-
-
-    def isValid(self, substr):
-        i = 0
-        j = len(substr) - 1
-        while i < j:
-            if substr[i] == substr[j]:
-                i = i + 1
-                j = j - 1
-            else:
-                return False
-        return True
+        res = []
+        n = len(s)
+        dfs([], 0)
+        return res
