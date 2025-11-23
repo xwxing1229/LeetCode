8e219b703a7ae6e7f1277dad283aba42b89c9a09
xwxing1229 <997427575@qq.com>
2025-11-23 16:53:36 +0900

    Should review

--- Cpp/1262_greatest_sum_divisible_by_three.cpp
+++ Cpp/1262_greatest_sum_divisible_by_three.cpp
@@ -1,18 +1,24 @@
 // https://leetcode.cn/problems/greatest-sum-divisible-by-three/
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        // dp[i][j]: maximum sum of elements chosen from nums[0], ..., nums[i-1] with
-        //           sum % 3 = j.
+        // dp[i][j]: maximum sum of elements chosen from nums[0], ..., nums[i-1]
+        //           with sum % 3 = j.
         int n = nums.size();
         vector<vector<int>> dp(n+1, vector<int>(3, -1));
         dp[0][0] = 0;
         for (int i = 0; i < n; ++i) {
+            int rem = nums[i] % 3;
             for (int j = 0; j < 3; ++j) {
-                int k = (3 + j - nums[i]%3) % 3;
-                if (dp[i][k] >= 0) {
-                    dp[i+1][j] = max(dp[i][j], dp[i][k] + nums[i]);
+                int match = (3 + j - rem) % 3;
+                if (dp[i][match] >= 0) {
+                    dp[i+1][j] = max(dp[i][j], dp[i][match] + nums[i]);
                 }
                 else {
                     dp[i+1][j] = dp[i][j];
@@ -22,3 +28,10 @@ public:
         return dp[n][0];
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3,6,5,1,8};
+    cout << sol.maxSumDivThree(nums) << '\n';
+    return 0;
+}
--- Python3/1262_greatest_sum_divisible_by_three.py
+++ Python3/1262_greatest_sum_divisible_by_three.py
@@ -1,21 +1,16 @@
 # https://leetcode.cn/problems/greatest-sum-divisible-by-three/
 
 class Solution:
-    def maxSumDivThree(self, nums):
-        """
-        Inputs:
-            nums: list[int]
-        Outputs:
-            res: int
-        """
-        # dp[i][j]: maximum sum of elements chosen from nums[0], ..., nums[i-1] with
-        #           sum % 3 = j.
+    def maxSumDivThree(self, nums: list[int]) -> int:
+        # dp[i][j]: maximum sum of elements chosen from nums[0], ..., nums[i-1]
+        #           with sum % 3 = j.
         dp = [[0, -1, -1] for i in range(len(nums)+1)]
         for i, num in enumerate(nums):
+            rem = num % 3
             for j in range(3):
-                k = (3 + j - num%3) % 3
-                if dp[i][k] >= 0:
-                    dp[i+1][j] = max(dp[i][j], dp[i][k]+num)
+                match = (3 + j - rem) % 3
+                if dp[i][match] >= 0:
+                    dp[i+1][j] = max(dp[i][j], dp[i][match]+num)
                 else:
                     dp[i+1][j] = dp[i][j]
-        return dp[len(nums)][0]
+        return dp[-1][0]
