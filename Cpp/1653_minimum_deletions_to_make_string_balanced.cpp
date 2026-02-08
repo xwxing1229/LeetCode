4ac7e40fca63d5d47afe2cd60421bc5daf01c998
xwxing1229 <997427575@qq.com>
2026-02-08 16:14:39 +0900

    Should review

--- Cpp/1653_minimum_deletions_to_make_string_balanced.cpp
+++ Cpp/1653_minimum_deletions_to_make_string_balanced.cpp
@@ -1,38 +1,34 @@
 // https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size();
-        vector<int> cnt_left_b(n), cnt_right_a(n);
-        for (int i = 1; i < n; ++i) {
-            // Count how many bs on the left side of the current position.
-            cnt_left_b[i] = cnt_left_b[i-1] + s[i-1] - 'a';
-            /* The following way may be slower.
-            if (s[i-1] == 'b') {
-                cnt_left_b[i] = cnt_left_b[i-1] + 1;
-            }
-            else {
-                cnt_left_b[i] = cnt_left_b[i-1];
-            } */
-
-            // Count how many as on the right side of the current position.
-            cnt_right_a[n-i-1] = cnt_right_a[n-i] + 1 - (s[n-i] - 'a');
-            /* The following way may be slower.
-            if (s[n-i] == 'a') {
-                cnt_right_a[n-i-1] = cnt_right_a[n-i] + 1;
-            }
-            else {
-                cnt_right_a[n-i-1] = cnt_right_a[n-i];
-            } */
+        int tmp = 0;
+        for (const char ch: s) {
+            tmp += (ch == 'a');
         }
-        int res = n;
-        for (int i = 0; i < n; ++i) {
-            int tmp = cnt_left_b[i] + cnt_right_a[i];
-            if (tmp < res) {
-                res = tmp;
+        int res = tmp;
+        for (const char ch: s) {
+            if (ch == 'a') {
+                tmp -= 1;
             }
+            else {
+                tmp += 1;
+            }
+            res = min(res, tmp);
         }
         return res;
     }
 };
+
+int main() {
+    Solution sol;
+    string s = "aababbab";
+    cout << sol.minimumDeletions(s) << '\n';
+    return 0;
+}
--- Python3/1653_minimum_deletions_to_make_string_balanced.py
+++ Python3/1653_minimum_deletions_to_make_string_balanced.py
@@ -1,30 +1,17 @@
 # https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/
 
 class Solution:
-    def minimumDeletions(self, s):
-        """
-        Inputs:
-            s: str
-        Outputs:
-            res: int
-        """
-        n = len(s)
-        cnt_left_b, cnt_right_a = [0 for _ in range(n)], [0 for _ in range(n)]
-        for i in range(1, n):
-            # Count how many bs on the left side of the current position.
-            if s[i-1] == "b":
-                cnt_left_b[i] = cnt_left_b[i-1] + 1
+    def minimumDeletions(self, s: str) -> int:
+        # Make all "a"s on the left side and all "b"s on the right side.
+        tmp = 0
+        for ch in s:
+            tmp += (ch == "a")
+        
+        res = tmp
+        for ch in s:
+            if ch == "b":
+                tmp += 1
             else:
-                cnt_left_b[i] = cnt_left_b[i-1]
-            # Count how many as on the right side of the current position.
-            if s[n-i] == "a":
-                cnt_right_a[n-i-1] = cnt_right_a[n-i] + 1
-            else:
-                cnt_right_a[n-i-1] = cnt_right_a[n-i]
-
-        res = n
-        for i in range(n):
-            tmp = cnt_left_b[i] + cnt_right_a[i]
-            if tmp < res:
-                res = tmp
+                tmp -= 1
+            res = min(res, tmp)
         return res
