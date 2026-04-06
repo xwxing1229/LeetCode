5be95db4feca16f768a18ad95d06a75b6b4c854b
xwxing1229 <997427575@qq.com>
2026-04-06 10:59:32 +0900

    Update

--- Cpp/0874_walking_robot_simulation.cpp
+++ Cpp/0874_walking_robot_simulation.cpp
@@ -1,39 +1,50 @@
 // https://leetcode.cn/problems/walking-robot-simulation/
 
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         unordered_set<int> obs;
-        for (auto &o: obstacles) {
-            obs.insert(o[0] + o[1] * 60001);
+        for (const auto &o: obstacles) {
+            obs.insert(o[0] * 60001 + o[1]);
         }
-        int res = 0, px = 0, py = 0, dx = 0, dy = 1;
-        for (auto command: commands) {
-            if (command == -2) {
+        int res = 0;
+        int px = 0, py = 0;
+        int dx = 0, dy = 1;
+        for (const auto &cmd: commands) {
+            if (cmd == -2) {
                 int tmp = dx;
                 dx = -dy;
                 dy = tmp;
             }
-            else if (command == -1) {
+            else if (cmd == -1) {
                 int tmp = dx;
                 dx = dy;
                 dy = -tmp;
             }
             else {
-                for (int i = 0; i < command; ++i) {
+                for (int i = 0; i < cmd; ++i) {
                     int x = px + dx, y = py + dy;
-                    if (obs.find(x + y * 60001) != obs.end()) {
-                        break;
-                    }
+                    if (obs.count(x * 60001 + y)) break;
                     px = x;
                     py = y;
-                    int tmp = px * px + py * py;
-                    if (tmp > res) {
-                        res = tmp;
-                    }
+                    res = max(res, px * px + py * py);
                 }
             }
         }
         return res;
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> commands = {4,-1,3};
+    vector<vector<int>> obstacles = {};
+    cout << sol.robotSim(commands, obstacles) << '\n';
+    return 0;
+}
--- Python3/0874_walking_robot_simulation.py
+++ Python3/0874_walking_robot_simulation.py
@@ -1,33 +1,22 @@
 # https://leetcode.cn/problems/walking-robot-simulation/
 
 class Solution:
-    def robotSim(self, commands, obstacles):
-        """
-        Inputs:
-            commands: list[int]
-            obstacles: list[list[int]]
-        Outputs:
-            res: int
-        """
+    def robotSim(self, commands: list[int], obstacles: list[list[int]]) -> int:
         obs = set()
         for o in obstacles:
-            obs.add(o[0] + o[1] * 60001)
+            obs.add(o[0] * 60001 + o[1])
         res = 0
         px, py = 0, 0
         dx, dy = 0, 1
-        for command in commands:
-            if command == -2:
-                tmp = dx
-                dx = -dy
-                dy = tmp
-            elif command == -1:
-                tmp = dx
-                dx = dy
-                dy = -tmp
+        for cmd in commands:
+            if cmd == -2:
+                dx, dy = -dy, dx
+            elif cmd == -1:
+                dx, dy = dy, -dx
             else:
-                for i in range(command):
+                for i in range(cmd):
                     x, y = px + dx, py + dy
-                    if x + y * 60001 in obs:
+                    if x * 60001 + y in obs:
                         break
                     px, py = x, y
                     res = max(res, px * px + py * py)
