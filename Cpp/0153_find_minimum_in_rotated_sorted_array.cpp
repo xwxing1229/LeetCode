93b166360472bf10fe0ed76b0c2fdd5f24d5b2c2
xwxing1229 <997427575@qq.com>
2026-05-15 11:03:46 +0900

    Update

--- Cpp/0153_find_minimum_in_rotated_sorted_array.cpp
+++ Cpp/0153_find_minimum_in_rotated_sorted_array.cpp
@@ -1,19 +1,32 @@
 // https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array/
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while (left < right) {
+        int res = nums[0];
+        int left = 0, right = nums.size()-1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            int val = nums[mid];
-            if (val <= nums[right]) {
+            res = nums[mid];
+            if (nums[mid] < nums[right]) {
                 right = mid;
             }
             else {
                 left = mid + 1;
             }
         }
-        return nums[left];
+        return res;
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3,4,5,1,2};
+    cout << sol.findMin(nums) << '\n';
+    return 0;
+}
--- Python3/0153_find_minimum_in_rotated_sorted_array.py
+++ Python3/0153_find_minimum_in_rotated_sorted_array.py
@@ -1,22 +1,14 @@
 # https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array/
 
 class Solution:
-    def findMin(self, nums):
-        """
-        Inputs:
-            nums: list[int]
-        Outputs:
-            res: int
-        """
-
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            mid = left + (right - left) // 2
-            val = nums[mid]
-
-            if val <= nums[right]:
+    def findMin(self, nums: list[int]) -> int:
+        res = nums[0]
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            res = nums[mid]
+            if nums[mid] < nums[right]:
                 right = mid
             else:
                 left = mid + 1
-        return nums[left]
+        return res
